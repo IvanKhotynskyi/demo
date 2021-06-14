4b769c9468353d9d193d516778aac750cf9d2fbd
IvanKhotynskyi <ivan.khotynskyi@gmail.com>
2021-06-14 12:28:11 +0300

    Demo project

---
+++ src/main/java/com/knowledgevision/demo/SimpleController.java
@@ -0,0 +1,16 @@
+package com.knowledgevision.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping(value = "/session/user-details", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getUser() {
+        return "{\"user_id\":20042,\"name\":\"Vadym Ocheretenyuk\",\"first_name\":\"Vadym\",\"last_name\":\"Ocheretenyuk\"," +
+                "\"email\":\"vadym.ocheretenyuk@openexc.com\",\"auth_provider\":\"Knowledge Vision\",\"secret_key\":\"a3jr6EimQYV5\"," +
+                "\"last_login\":\"2021-06-14T08:10:01Z\",\"user_agreement\":true,\"roles\":[{\"role_id\":20,\"role_name\":\"System Administrator\"}]}";
+    }
+}
